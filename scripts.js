6e178507ba7d29c5fe824830ef1ce25e897c2069
adilafsar99 <adilafsar99@gmail.com>
2026-03-01 16:30:38 +0500

    Write down the psuedocode for generating computer's choice in comment form

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/* Declare a function named getComputerChoice to randomly generate and return
+ a string 'Rock', 'Paper' or 'Scissors'. */
+
+/* Declare a variable named comChoiceNum to store a number between 0 and 2 to select the
+computer's choice */
+
+/* Declare a variable named comChoice to store computer's choice and initialize
+ it with an empty string. */
+
+/* Generate a random whole number between 0 & 2 and store it inside the variable comChoiceNum. */
+
+// Compare the value stored inside comChoiceNum  and:
+// If comChoiceNum = 0, set comChoice to 'Rock'.
+// Else if comChoiceNum = 1, set comChoice to 'Paper'.
+// Else if comChoiceNum = 2, set comChoice to 'Scissors'.
+// Retrieve comChoice from the function.
