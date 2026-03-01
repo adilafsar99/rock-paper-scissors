4211e88c088299ae9c628a2e0f7d16c98e3a0487
adilafsar99 <adilafsar99@gmail.com>
2026-03-01 21:00:26 +0500

    Transfer the function to get computer and user choices outside of the playGame() function.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
+// Declare a function to randomly generate and return the computer's choice.
+
+function getComputerChoice() {
+    // Declare a variable named comChoiceNum to store a number to select the computer's choice.
+    let comChoiceNum;
+    // Declare a variable named comChoice to store the  computer's choice.
+    let comChoice = '';
+
+    // Generate a random whole number between 0 & 2 and store it inside the variable comChoiceNum.
+    comChoiceNum = Math.floor((Math.random() * 3));
+
+    // Compare the value of comChoiceNum with 0 and 1 to determine the value of comChoice.
+    if (comChoiceNum === 0) {
+        comChoice = 'rock';
+    }
+    else if (comChoiceNum === 1) {
+        comChoice = 'paper';
+    }
+    else {
+        comChoice = 'scissors';
+    }
+    return comChoice;
+}
+
+// Declare a function to prompt the user to enter their choice and return it.
+
+function getUserChoice() {
+    // Prompt the user to enter their choice and store it inside the variable userChoice
+    let userChoice = prompt('Enter your choice: ', '');
+
+    // Convert the value inside userChoice to lower case to remove case related errors
+    userChoice = userChoice.toLowerCase()
+    return userChoice;
+}
+
 /* Declare a function that simulates 5 rounds of Rock, Paper, Scissors between the computer
 and the user. */
 
@@ -8,43 +43,8 @@ function playGame() {
     // Initialize a variable to store the computer's score.
     let computerScore = 0;
 
-
-    // Declare a function to randomly generate and return the computer's choice.
-
-    function getComputerChoice() {
-        // Declare a variable named comChoiceNum to store a number to select the computer's choice.
-        let comChoiceNum;
-        // Declare a variable named comChoice to store the  computer's choice.
-        let comChoice = '';
-
-        // Generate a random whole number between 0 & 2 and store it inside the variable comChoiceNum.
-        comChoiceNum = Math.floor((Math.random() * 3));
-
-        // Compare the value of comChoiceNum with 0 and 1 to determine the value of comChoice.
-        if (comChoiceNum === 0) {
-            comChoice = 'rock';
-        }
-        else if (comChoiceNum === 1) {
-            comChoice = 'paper';
-        }
-        else {
-            comChoice = 'scissors';
-        }
-        return comChoice;
-    }
-
-    console.log(getComputerChoice());
-
-    // Declare a function to prompt the user to enter their choice and return it.
-
-    function getUserChoice() {
-        // Prompt the user to enter their choice and store it inside the variable userChoice
-        let userChoice = prompt('Enter your choice: ', '');
-
-        // Convert the value inside userChoice to lower case to remove case related errors
-        userChoice = userChoice.toLowerCase()
-        return userChoice;
-    }
+    // Initialize a variable to keep track of the rounds.
+    let round = 1;
 
     // Declare a function that takes the user and computer's choice and simulates a round.
 
